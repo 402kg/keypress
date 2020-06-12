3fa6f8549aa8d8b18efca9eadd819a8b2e818405
Iakov Salikov <isalikov@pm.me>
2020-06-13 02:19:16 +0300

    feat: deploy scripts

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # keypress
 
-...
+Online tool for debug browser key events
---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+const sftp = require('gulp-sftp-up4');
+
+gulp.task('deploy', function () {
+    return gulp.src('web/**/*')
+        .pipe(sftp({
+            host: 'keypress.tk',
+            keyContents: process.env.ROOT_KEY,
+            remotePath: '/var/www/keypress',
+            user: 'root',
+            remotePlatform: 'unix',
+        }));
+});
